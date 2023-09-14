8b378763f339b968c02aa43bf7476a55c49eab3d
Artur Lisetskyi <126811047+weaaves@users.noreply.github.com>
2023-09-14 14:01:21 +0300

    Add files via upload

--- js/customer-slider.js
+++ js/customer-slider.js
@@ -1,14 +1,16 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 20,
   breakpoints: {
     768: {
-      slidesPerView: 3,
+      slidesPerView: 2,
     },
     // when window width is >= 640px
     1280: {
       slidesPerView: 3,
     }
     },
-  
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
